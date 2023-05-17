9417502055b6cb4b181a39ab727222717bec6dfa
Prasad <theisn@upmc.edu>
2023-05-17 10:08:08 -0400

    adding configuraiton scripts

--- Example_ObjectiveFunction.m
+++ Example_ObjectiveFunction.m
@@ -1,1 +1,9 @@
-%Example_ObjectiveFunction
+%% Example_ObjectiveFunction
+% for references and additional information, see README.md
+% By: University of Pittsburgh CONCEPT LAB 2023
+
+%% Configure the workspace and search paths
+addpath('utilities');
+
+%% Load data
+
--- utilities/Configure_LFR_sim.m
+++ utilities/Configure_LFR_sim.m
@@ -1,8 +1,13 @@
-UtilitiesPath='/data1/Utilities';
-addpath(UtilitiesPath);
-NMIpath=[UtilitiesPath '/community_nmi'];
+utilitiesPath='utilities';
+%addpath(UtilitiesPath);
+NMIpath=[utilitiesPath '/NMI'];
 addpath(NMIpath);
-BCTpath = [UtilitiesPath '/BCT/2017_01_15_BCT'];
+BCTpath = [utilitiesPath '/BCT'];
 addpath(BCTpath);
+DATApath = [utilitiesPath '/data'];
+addpath(DATApath);
 
-clear UtilitiesPath NMIpath BCTpath
+data=[DATApath '/LFR_sim_data_20220622.mat']
+load(D
+
+clear utilitiesPath NMIpath BCTpath
